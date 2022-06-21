26bb54717de55483c3296dace17a05e60ac91d91
Aleksei Syshchenko <syshenco@gmail.com>
2022-06-21 10:28:03 +0400

    First version tcp client

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter ip address (for exit enter q): ");
+            int port = 80;
+            string server = Console.ReadLine();
+            while (server != "q" && server != "Q")
+            {
+                try
+                {
+                   TcpClient client = new TcpClient();
+                   client.Connect(server, port); 
+                   Console.WriteLine("Connection establised");
+                   client.Close();
+                   Console.WriteLine("Connection closed");
+                   Console.WriteLine();
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+
+                
+                Console.Write("Enter ip address for new connection (or q): ");
+                server = Console.ReadLine();
+        
+            }
+        }
+    }
+}
