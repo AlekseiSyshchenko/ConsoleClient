89d479b698e3766eedebe5db1da162d07d687662
AlekseiSyshchenko <syshenco@gmail.com>
2022-06-24 10:46:14 +0400

    Использовал System.IO.StreamWriter

--- Program.cs
+++ Program.cs
@@ -8,40 +8,38 @@ namespace ConsoleClient
         const string ip = "87.250.250.242";
         static void Main(string[] args)
         {
-            
-            
-               int closedPort = 0; 
 
-                
-                
-                   //String fileName = $"report{server}.csv";
-                   //FileInfo fileInfo = new FileInfo(@$"report{ip}.csv");
-                   //StreamWriter report = fileInfo.CreateText();
-                   File.Create($"report{ip}.csv");
-                
-                
-                try
+
+            int closedPort = 0;
+            string path = @"c:\report.csv";
+            System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+
+            try
+            {
+
+                TcpClient client = new TcpClient();
+
+                for (int port = 1; port <= 65535; port++)
                 {
-                   
-                   TcpClient client = new TcpClient();
-                   for(int port = 1; port <= 65535; port++)
-                   {
-                        closedPort = port;
-                        client.Connect(ip, port); 
-                        //report.WriteLine($"{DateTime.Now}, {ip}, {port}, Open");
-                        File.WriteAllText($"report{ip}.csv", $"{DateTime.Now}, {ip}, {port}, Open");
-                        client.Close();
-                   }
-                   
+                    Console.WriteLine($"Я в цикле {port}");
+                    closedPort = port;
+                    client.Connect(ip, port);
+                    Console.WriteLine("Open");
+                    file.WriteLine($"{DateTime.Now}, {ip}, {port}, Open");
+                    client.Close();
                 }
-                catch (SocketException e)
-                {
-                    File.WriteAllText($"report{ip}.csv", $"{DateTime.Now}, {ip}, {closedPort}, Close");
-                    //report.WriteLine($"{DateTime.Now}, {ip}, {closedPort}, Close");
-                    //Console.WriteLine("SocketException: {0}", e);
-                }
+
+            }
+
+            catch (SocketException e)
+            {
+                Console.WriteLine($"Я в catch {closedPort}");
+                Console.WriteLine("Close");
+                file.WriteLine($"{DateTime.Now}, {ip}, {closedPort}, Close");
+
+            }
         }
     }
 }
-    
+
 
