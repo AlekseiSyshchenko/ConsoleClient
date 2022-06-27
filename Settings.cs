ec39b5d441678c6aa78009395ec2dbd0d3b4f9fa
AlekseiSyshchenko <syshenco@gmail.com>
2022-06-27 16:26:45 +0400

    Ver 0.2

--- Settings.cs
+++ Settings.cs
@@ -3,6 +3,6 @@
 public static class Settings
 {
      public static string ip = "87.250.250.242";
-     public static string path = @$"c:\report{Settings.ip}.csv";
+     public static string path = @$"c:\report{DateTime.Now.Day}{DateTime.Now.Month}{DateTime.Now.Year}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.csv";
      
 }
